5499769810f0a9bd17693ed588adfb0a29ba9342
myleen12 <myleen.santana12@icloud.com>
2023-06-07 23:17:12 -0700

    Added password

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ require("console.table")
 var connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+    
+    port: 3001,
 
     // Your username
     user: "root",
 
     // Your password
-    password: "",
+    password: "q",
     database: "employees_db"
 });
 
